9fd82dd99a55d8078eb8be7e2cf92a82ef6d4a27
Dakota <dakotadunn5@gmail.com>
2024-02-02 21:13:16 -0800

    started writing parser for include statements, wrote test file in debug for parsing in the future. TODO: write gauntmod_parser to parse and sync changes

---
+++ debug/include_test.cpp
@@ -0,0 +1,3 @@
+#include <a.out.h>
+#include <ar.h>
+#include <argp.h>
--- include/gaunt_include.h
+++ include/gaunt_include.h
@@ -2,28 +2,35 @@
 #include <stdio.h>
 #include <iostream>
 
-static char currentToken[4096];
-static size_t currentTokenLength;
+namespace gaunt_parser{
+    static char currentToken[4096];
+    static size_t currentTokenLength;
 
-void addToken (char c){
-    if (currentTokenLength < sizeof(currentToken)) {
-        currentToken[currentTokenLength] = c;
+    void addToken(char c)
+    {
+        if (currentTokenLength < sizeof(currentToken))
+        {
+            currentToken[currentTokenLength] = c;
+        }
     }
+
+    void printToken()
+    {
+        std::cout << currentToken << (int)currentTokenLength << std::endl;
+    }
+
+    typedef enum
+    {
+        STATE_FindStartOfData,
+        STATE_FindStartOfToken,
+        STATE_ParseNumber,
+        STATE_ParseString,
+        STATE_CheckEndOfString,
+        STATE_FindDelimiter,
+        STATE_ParseError,
+        STATE_EndOfData
+    } TokenType;
+
+    TokenType tokenType = STATE_FindStartOfData; 
 }
-
-void printToken(){
-    std::cout << currentToken << (int)currentTokenLength << std::endl;
-}
-
-typedef enum {
-    TOKEN_TYPE_NONE,
-    TOKEN_TYPE_NUMBER,
-    TOKEN_TYPE_OPERATOR,
-    TOKEN_TYPE_IDENTIFIER,
-    TOKEN_TYPE_STRING,
-    TOKEN_TYPE_KEYWORD,
-    TOKEN_TYPE_PUNCTUATOR,
-    TOKEN_TYPE_PARSE_ERROR
-} TokenType;
-
 // TODO: implement #include tokenization and parsing here
