c14c412d6971e4f23ff4cbe3e6c1b2f921356144
Dakota Dunn <dakotadunn5@gmail.com>
2024-02-06 23:07:47 -0500

    fix ODR violations and make this code cleaner, consider refactoring entirely and just writing this in C

--- include/gaunt_include.h
+++ include/gaunt_include.h
@@ -1,73 +1,64 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
-namespace gaunt_include{
-    static char currentToken[4096];
-    static size_t currentTokenLength;
+//BUG: some issue with function definition ODR rules, have to fix this going forward
+namespace gaunt_include {
+static char currentToken[4096];
+static size_t currentTokenLength;
 
-    void addToken(char c)
-    {
-        if (currentTokenLength < sizeof(currentToken))
-        {
-            currentToken[currentTokenLength] = c;
-        }
-    }
+void addToken(char c) {
+  if (currentTokenLength < sizeof(currentToken)) {
+    currentToken[currentTokenLength] = c;
+  }
+}
 
-    void printToken()
-    {
-        std::cout << currentToken << (int)currentTokenLength << std::endl;
-    }
+void printToken() {
+  std::cout << currentToken << (int)currentTokenLength << std::endl;
+}
 
-    typedef enum
-    {
-        STATE_FindStartOfData,
-        STATE_FindStartOfToken,
-        STATE_ParseNumber,
-        STATE_ParseString,
-        STATE_CheckEndOfString,
-        STATE_FindDelimiter,
-        STATE_ParseError,
-        STATE_EndOfData
-    } TokenType;
+enum TokenType {
+  STATE_FindStartOfData,
+  STATE_FindStartOfToken,
+  STATE_ParseNumber,
+  STATE_ParseString,
+  STATE_CheckEndOfString,
+  STATE_FindDelimiter,
+  STATE_ParseError,
+  STATE_EndOfData
+};
 
-    TokenType tokenType = STATE_FindStartOfData; 
+TokenType token = STATE_FindStartOfData;
 
-    void parseData(char c)
-    {
-      while (tokenType != STATE_ParseError && tokenType != STATE_EndOfData)
-      {
-        int c = fgetc(stdin);
-        if (c == -1)
-        {
-            switch (tokenType)
-            {
-            case STATE_ParseNumber:
-            case STATE_CheckEndOfString:
-                printToken();
-                //FIXME: a value of type "enum gaunt_include::<unnamed>" cannot be assigned to an entity of type "gaunt_include::TokenType"C/C++(513)
-                // this error shouldn't be happening here, not sure why, maybe because using namespace?
-                tokenType = STATE_EndOfData;
-                break;
-            case STATE_ParseString:
-                std::cout << "Error: Unterminated string" << std::endl;
-                tokenType = STATE_ParseError;
-                break;
-            case STATE_FindStartOfData:
-            case STATE_FindStartOfToken:
-            case STATE_FindDelimiter:
-                tokenType = STATE_EndOfData;
-                break;
-            case STATE_ParseError:
-            case STATE_EndOfData:
-                break;
-            }
-        }
-        switch (tokenType)
-        {
-
-        }
-
+void parseData(char c) {
+  while (token != STATE_ParseError && token != STATE_EndOfData) {
+    int c = fgetc(stdin);
+    if (c == -1) {
+      switch (token) {
+      case STATE_ParseNumber:
+      case STATE_CheckEndOfString:
+        printToken();
+        // FIXME: a value of type "enum gaunt_include::<unnamed>" cannot be
+        // assigned to an entity of type "gaunt_include::TokenType"C/C++(513)
+        //  this error shouldn't be happening here, not sure why, maybe because
+        //  using namespace?
+        token = STATE_EndOfData;
+        break;
+      case STATE_ParseString:
+        std::cout << "Error: Unterminated string" << std::endl;
+        token = STATE_ParseError;
+        break;
+      case STATE_FindStartOfData:
+      case STATE_FindStartOfToken:
+      case STATE_FindDelimiter:
+        token = STATE_EndOfData;
+        break;
+      case STATE_ParseError:
+      case STATE_EndOfData:
+        break;
       }
     }
+    switch (token) {}
+  }
 }
+} // namespace gaunt_include
